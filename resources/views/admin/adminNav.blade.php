a9e8f139850624cbd7bfb880aab1ac014e6aa117
Marin <marin.hysollari@cit.edu.al>
2017-10-03 12:38:17 +0200

    fixing home route

--- resources/views/admin/adminNav.blade.php
+++ resources/views/admin/adminNav.blade.php
@@ -1,6 +1,9 @@
-<a href="{{route('feeds.index')}}" class="logo">
+<a href="{{route('feeds.index')}}" class="logo" style="padding-top: 10px;">
 
-        <span class="logo"><img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/137/316242137_695ddda0-ff3d-4e78-9899-3d65487ae80f.png?cb=1507023955" class="img-responsive"></span>
+    <!-- logo for regular state and mobile devices -->
+
+    <span class="logo-lg"><img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/137/316242137_695ddda0-ff3d-4e78-9899-3d65487ae80f.png?cb=1507023955" class="img-responsive"></span>
+
 
 </a>
 @if(count(\App\User::all()) != 0)
