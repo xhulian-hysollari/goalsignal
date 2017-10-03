604b6a56f3825d547984b4db2937b06d56bd06cc
Marin <marin.hysollari@cit.edu.al>
2017-10-03 14:05:29 +0200

    fixing home route

--- resources/views/admin/adminNav.blade.php
+++ resources/views/admin/adminNav.blade.php
@@ -1,9 +1,7 @@
 <a href="{{route('feeds.index')}}" class="logo" style="padding-top: 10px;">
 
     <!-- logo for regular state and mobile devices -->
-
-    <span class="logo-lg"><img src="{{asset('images/logo.jpg')}}" class="img-responsive"></span>
-
+    <span class="logo-lg"><img src="{{asset('images/logo.jpg')}}" class="img-responsive xs-hidden sm-hidden"></span>
 
 </a>
 @if(count(\App\User::all()) != 0)
@@ -13,6 +11,8 @@
     <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
     </a>
+
+    <span class="logo-lg"><img src="{{asset('images/logo.jpg')}}" class="img-responsive md-hidden lg-hidden"></span>
 
 </nav>
 @endif
