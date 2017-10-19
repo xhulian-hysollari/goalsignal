539f15bbe0cab2cf70a3f77cdad7955075f00440
Marin <erkid.hoxholli@inovap.com>
2017-10-19 09:44:01 +0200

    fixing home route

---
+++ database/migrations/2017_10_19_073335_change_feeds_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feeds', function (Blueprint $table) {
+            $table->string('locale');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feeds');
+    }
+}
