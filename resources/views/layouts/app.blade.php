5180aa74214fe3f0d9327ffd034c1624dbea9a7e
Marin <erkid.hoxholli@inovap.com>
2017-10-24 14:11:11 +0200

    fixing home route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,14 @@
 
     <title>GOALSignal</title>
 
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-6040717899325039",
+            enable_page_level_ads: true
+        });
+    </script>
+
 </head>
 <body>
 <header>
