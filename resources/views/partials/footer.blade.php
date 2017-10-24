7dc873ea6d0a7d80c6160a45ebb26f6de677dace
Marin <erkid.hoxholli@inovap.com>
2017-10-24 14:38:57 +0200

    fixing home route

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,29 +2,27 @@
     <div class="container">
         <div class="row">
 
+            @if(isset($categories) && count($categories)>0)
+                <div class="col-sm-2 footer-box">
+                    <h3 class="wiget-title">{{trans('admin.categories')}}</h3>
+                    <ul class="menu-services">
+                            @foreach($categories as $category)
+                                <li><a href="{{url('feeds/showCategory', $category->url)}}">{{ucwords($category->name)}}</a></li>
+                            @endforeach
+                    </ul>
+                </div>
+            @endif
 
-            <div class="col-sm-2 footer-box">
-                <h3 class="wiget-title">{{trans('admin.categories')}}</h3>
-                <ul class="menu-services">
-                    @if(isset($categories) && count($categories)>0)
-                        @foreach($categories as $category)
-                            <li><a href="{{url('feeds/showCategory', $category->url)}}">{{ucwords($category->name)}}</a></li>
-                        @endforeach
-                    @endif
-                </ul>
-            </div>
-
-            <div class="col-sm-2 footer-box">
-                <h3 class="wiget-title">{{trans('admin.pages')}}</h3>
-                <ul class="menu-services">
-                    @if(isset($pages) && count($pages)>0)
+            @if(isset($pages) && count($pages)>0)
+                <div class="col-sm-2 footer-box">
+                    <h3 class="wiget-title">{{trans('admin.pages')}}</h3>
+                    <ul class="menu-services">
                         @foreach($pages as $page)
                             <li><a href="{{url('dashboard/pages/show', $page->slug)}}">{{ucwords($page->title)}}</a></li>
                         @endforeach
-                    @endif
-                </ul>
-            </div>
-
+                    </ul>
+                </div>
+            @endif
         </div>
     </div>
 </footer>
