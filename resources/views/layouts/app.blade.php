e433ab973e567984d35af9490fa778a2aa8422ef
Marin <erkid.hoxholli@inovap.com>
2017-12-23 17:02:29 +0100

    Removing all the languages except english.. check:
    1. Signal/config/laravellocalization.php => remove comments from 'sq'
    2. views/admin/adminSideBar => remove the comments to add back the choose language option
    3. views/partials/navbar => remove the comments to add back the choose language option

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,18 +82,19 @@
             <!-- END OF /. MAIN CONTENT -->
             @include('partials.sideContent')
         </div>
-    </div>
 
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-format="fluid"
-         data-ad-layout-key="-fe+6b+2x-jx+pw"
-         data-ad-client="ca-pub-6040717899325039"
-         data-ad-slot="2289866083"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-format="fluid"
+             data-ad-layout-key="-fe+6b+2x-jx+pw"
+             data-ad-client="ca-pub-6040717899325039"
+             data-ad-slot="2289866083"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+
+    </div>
 
 </main>
 
