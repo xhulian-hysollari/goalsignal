458fe957f32c35c3530a034c801161d2ac117a52
Xhulian <erkid.hoxholli@inovap.com>
2018-01-15 11:24:47 +0100

    remove adds

--- resources/views/partials/sideContent.blade.php
+++ resources/views/partials/sideContent.blade.php
@@ -96,7 +96,7 @@
                     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                     <!-- ADD2 -->
                     <ins class="adsbygoogle"
-                         style="display:inline-block;width:300px;height:600px"
+                         style="display:inline-block;width:100%;height:300px"
                          data-ad-client="ca-pub-6040717899325039"
                          data-ad-slot="5466747669"></ins>
                     <script>
